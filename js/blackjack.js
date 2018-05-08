ad96b0131d0d1167d1076667dfbb6dba19ad8fec
Ryan Hikel <hikelryan@ool-ad020716.dyn.optonline.net>
2018-05-08 14:38:56 -0400

    shuffled deck

--- js/blackjack.js
+++ js/blackjack.js
@@ -2,18 +2,14 @@ const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 const suits = ['♠️', '♣️', '♥️', '♦️'];
 let deck = [];
 let startingDeck = [];
+let shuffledDeck = [];
+
+//  jQuery
 const $body = $('body');
 const $hit = $('.hit-me');
 const $stay = $('.stay');
 
 function buildDeck() {
-  //   $.each(values, function (key, value) {
-  //     deck.push({
-  //       value: cardValues,
-  //       suit: cardSuits,
-  //     });
-
-
   for (let cardValues of values) {
     for (let cardSuits of suits) {
       deck.push({
@@ -24,5 +20,17 @@ function buildDeck() {
     startingDeck = deck.slice(0);
   }
 }
+function shuffleDeck() {
+  while (shuffledDeck.length < deck.length) {
+    let randomCard = Math.floor(Math.random() * 52);
+    let moveCard = deck[randomCard];
+    if (moveCard) {
+      deck.splice(randomCard, 1, null);
+      shuffledDeck.push(moveCard);
+    }
+  }
+  deck = shuffledDeck;
+}
 buildDeck();
+shuffleDeck();
 console.table(deck);
