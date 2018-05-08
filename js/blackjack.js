64cd61aa997f626638bf75ec9777e1eae1946203
Ryan Hikel <hikelryan@ool-ad020716.dyn.optonline.net>
2018-05-08 14:30:54 -0400

    built deck

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,1 +1,28 @@
-console.log('hi');
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+const suits = ['♠️', '♣️', '♥️', '♦️'];
+let deck = [];
+let startingDeck = [];
+const $body = $('body');
+const $hit = $('.hit-me');
+const $stay = $('.stay');
+
+function buildDeck() {
+  //   $.each(values, function (key, value) {
+  //     deck.push({
+  //       value: cardValues,
+  //       suit: cardSuits,
+  //     });
+
+
+  for (let cardValues of values) {
+    for (let cardSuits of suits) {
+      deck.push({
+        value: cardValues,
+        suit: cardSuits,
+      });
+    }
+    startingDeck = deck.slice(0);
+  }
+}
+buildDeck();
+console.table(deck);
